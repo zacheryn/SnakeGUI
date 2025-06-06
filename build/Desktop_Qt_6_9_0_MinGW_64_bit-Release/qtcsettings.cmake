049ba6393fb542a9aee93b6d581e6bbb82d96e38
Zachery Nagy <zacheryn@umich.edu>
2025-06-06 19:07:41 -0400

    Updated .gitignore

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/qtcsettings.cmake
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/qtcsettings.cmake
@@ -1,11 +1,11 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_C_COMPILER" "C:/Qt/Tools/mingw1310_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.9.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/Qt/6.9.0/mingw_64" CACHE "PATH" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/ZakNa/source/QT/Snake/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
 set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.9.0/mingw_64" CACHE "PATH" "" FORCE)
 set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
 set("CMAKE_CXX_COMPILER" "C:/Qt/Tools/mingw1310_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.9.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qt/Tools/mingw1310_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/ZakNa/source/QT/Snake/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
 set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
